f8398e280237d6e220f5ed2b7180bbddb62db828
Andrew Zhao <andrewzhao@berkeley.edu>
2020-10-23 17:40:54 -0700

    fixed typo

--- dino_chrome_ext/background.js
+++ dino_chrome_ext/background.js
@@ -10,6 +10,13 @@ window.addEventListener('offline', (event) => {
   } 
 });
 
+chrome.browserAction.onClicked.addListener(function(tab){
+  chrome.windows.create({
+    // Just use the full URL if you need to open an external page
+    url: chrome.runtime.getURL("ALPHA_TEST/index.html"),
+    type: "popup"
+  });
+});
 
 
 
